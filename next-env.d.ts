8af0d4ae46c98c29c66930cf2dc37c6414d4bbf3
Akira Yamanashi <me@jameslin.info>
2026-09-12 16:57:12 -0400

    atp im just procrastinatin™

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+import "vinext/types/augmentations";
+import "./.next/types/routes.d.ts";
+
+// see https://nextjs.org/docs/app/api-reference/config/typescript
