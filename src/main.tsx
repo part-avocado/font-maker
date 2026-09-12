a571aab3deddb6f9c1fde297d98352d9732e0c3d
Akira Yamanashi <me@jameslin.info>
2026-09-12 15:14:19 -0400

    ts is awful code

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,6 @@
 import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client'
+import {createRoot} from 'react-dom/client';
+import Home from '../app/page';
+import '../app/globals.css';
+
+createRoot(document.getElementById('root')!).render(<StrictMode><Home /></StrictMode>,);
