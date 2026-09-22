aa3df9ff032ba3067f560fdd1ff4ad4e955d9ff8
Akira Yamanashi <me@jameslin.info>
2026-09-22 10:37:12 -0400

    larping to have organization

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+// external package
 import {ComponentProps, PointerEvent, useEffect, useMemo, useRef, useState} from 'react';
 import {Minus, Plus, SlidersHorizontal, Type} from 'lucide-react';
-import { Tool, Variation, basePairLatin, digitChars, googleFontsCatalog, punctuationChars, variations } from './lib/constants';
 
-// TODO: implement layer cloning, then add
+// ./lib/*
+import { Tool, Variation, basePairLatin, digitChars, googleFontsCatalog, punctuationChars, variations } from './lib/constants';
+import {download} from './lib/download'
+import { exportSvg } from './lib/svg-export';
 import { Grid, blank, defaultLetter, dilate, resample,seed, shear, validGrid } from './lib/grid';
+
+// ./hooks/*
+
+// ./components/*
